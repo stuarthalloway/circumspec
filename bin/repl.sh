98986f0597f9b983b5cc85fb238f83afc51bb0b4
(why?) <team@oredev.org>
2009-11-05 15:34:05 +0100

    minimal ant build, one more reordering

--- bin/repl.sh
+++ bin/repl.sh
@@ -1,1 +1,1 @@
-java -cp src:test:lib/jline-0.9.94.jar:lib/clojure.jar:lib/clojure-contrib.jar jline.ConsoleRunner clojure.main -i src/repl.clj -r
+java -cp src:test:lib/jline-0.9.94.jar:lib/clojure.jar:lib/clojure-contrib.jar jline.ConsoleRunner clojure.main -i bin/repl.clj -r
