d6991ca347a753d74adf2a53e00d42e74ed85e26
Stuart Halloway <stu@thinkrelevance.com>
2010-01-20 09:08:28 -0500

    bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circumspec "0.0.3"
+(defproject circumspec "0.0.4"
   :dependencies [[org.clojure/clojure
                   "1.1.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib
