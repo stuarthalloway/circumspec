b9fb2da756a9f053265c463b4d3b9864fef36bf6
Stuart Halloway <stu@thinkrelevance.com>
2010-03-18 13:27:15 -0400

    let there be lein compilation

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
                  [jline
                   "0.9.94"]]
   :dev-dependencies [[swank-clojure
-                  "1.1.0"]]
+                      "1.1.0"]]
+  :namespaces [circumspec.AssertFailed]
   :repositories {"clojure-releases" "http://build.clojure.org/releases"})
