a7ba0c08d261703b7390d3767f1ea63ae76cdd85
Stuart Halloway <stu@thinkrelevance.com>
2010-04-30 12:54:12 -0400

    freeze to clojure past chunk fix

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject circumspec "0.0.10"
+(defproject circumspec "0.0.12"
   :dependencies [[org.clojure/clojure
-                  "1.2.0-master-20100430.130229-58"]
+                  "1.2.0-master-20100430.160229-59"]
                  [org.clojure/clojure-contrib
-                  "1.2.0-20100430.130503-89"]
+                  "1.2.0-20100430.160503-90"]
                  [jline
                   "0.9.94"]]
   :dev-dependencies [[swank-clojure
